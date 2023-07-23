3913d424b396f3badd26cf21f14e5bd49ae80005
RedStormBT <louanbelicaud63@gmail.com>
2023-07-23 14:45:00 +0200

    close menu on click outside of the opened menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -9,9 +9,28 @@ export class HeaderComponent {
   isMenuOpen: boolean = false;
   isShrink: boolean = false;
   shrinkOffset: number = 200;
+  @ViewChild('menu') menu: ElementRef | undefined;
 
-  toggleMenu(): void {
+  handleMenuClick(): void {
+    // Close the menu on click
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  handleLinkClick(): void {
+    // Close the menu on click
+    this.isMenuOpen = !this.isMenuOpen;
+    // Scroll to the top of the page on click of nav elements
+    if (!this.isMenuOpen) {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event) {
+    if (!this.menu?.nativeElement.contains(event.target) && this.isMenuOpen) {
+      this.isMenuOpen = false;
+    }
   }
 
   @HostListener('window:scroll', [])
