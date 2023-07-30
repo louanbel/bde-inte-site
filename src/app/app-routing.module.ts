fc7557e868c0a09436e1989c042a785124f99696
enolabest44 <enora.le-bras@imt-atlantique.net>
2023-07-30 11:48:16 +0200

    creation page rentree

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { IntegrationComponent } from './views/integration/integration.component'
 import { CampusComponent } from './views/campus/campus.component';
 import { WeiComponent } from './views/wei/wei.component';
 import { AssosClubComponent } from './views/assos-club/assos-club.component';
+import { RentreeComponent } from './views/rentree/rentree.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -12,6 +13,8 @@ const routes: Routes = [
   { path: 'campus', component: CampusComponent },
   { path: 'wei', component: WeiComponent },
   { path: 'assos-et-club', component: AssosClubComponent },
+  { path: 'rentree', component: RentreeComponent },
+
 ];
 
 @NgModule({
